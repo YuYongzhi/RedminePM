47eba8d96474997ccef52143aa49a4c20d1a16dc
YongZhi.Yu <yongzhi.yu@maitao.com>
2017-09-13 15:41:23 +0800

    初始化仓库

---
+++ app/src/main/java/com/maitao/redmine/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.maitao.redmine.base;
+
+/**
+ * <p>
+ * - Created by: maitao.
+ * <br>
+ * -       Date: 17-9-13.
+ */
+
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/maitao/redmine/setting/SeetingActivity.java
@@ -0,0 +1,11 @@
+package com.maitao.redmine.setting;
+
+/**
+ * <p>
+ * - Created by: maitao.
+ * <br>
+ * -       Date: 17-9-13.
+ */
+
+public class SeetingActivity {
+}
